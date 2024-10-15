26b5da594e63cba122fa6f9d17e46510c3f15bb8
Adetola Olukoya <adetola442@gmail.com>
2024-10-15 10:34:03 -0500

    commentcard commit

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -1,15 +1,33 @@
-import React, { useContext, useEffect, useMemo, useRef } from 'react'
+import React, { useContext, useEffect, useMemo, useRef, useState } from 'react'
 import "./CommentCard.scss"
 import anonymous from "../../../public/anonymous.jpg"
 import { StanzaContext } from '../../StanzaContext'
 import { useNavigate } from 'react-router'
 
 const CommentCard = ({ comment }) => {
-
+const [hasProfilePic, setHasProfilePic] = useState()
+  const [url, setUrl] = useState()
   const { user, profilePicForPoem} = useContext(StanzaContext)
   const navigate = useNavigate()
     
-    
+
+  let value = profilePicForPoem.find((url) => {
+    return url.includes(comment.userId)
+  })
+
+  
+
+    useEffect(() =>{
+        if(value){
+            setHasProfilePic(true)
+            setUrl(value)
+        } else{
+            setHasProfilePic(false)
+        }
+        
+
+    },[value]) 
+
   useEffect(() => {
       if(!user){
           navigate("/")
@@ -22,14 +40,17 @@ const CommentCard = ({ comment }) => {
         
         <div className="commentCardTop">
             <p>{comment.userEmail}</p>
-            {profilePicForPoem.map((url,index) => (
-                url.includes(comment.userId) 
-                && <img src={url} alt="" key={index}/>
-                
-            ))}
-            {/*profilePic.includes(comment.userId) 
-            ? <img src={profilePic} alt="" />
-            : <img src={anonymous} alt="" />*/}
+            {
+            hasProfilePic 
+            ?
+            (
+            <img src={url} alt=""/>)
+            
+            :
+            (
+                <img src={anonymous} alt=""/>
+            )
+            }
             
         </div>
         <p>{comment.comment}</p>
